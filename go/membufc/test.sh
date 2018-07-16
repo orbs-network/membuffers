b190f063d467bdb784cc352c03f94b6adb24e09c
Tal Kol <talkol@gmail.com>
2018-07-16 19:12:28 +0300

    Closes #12: e2e tests should first generate sources for SUT

--- go/membufc/test.sh
+++ go/membufc/test.sh
@@ -1,26 +1,8 @@
-go run *.go ./e2e/protos/transaction.proto
-go run *.go ./e2e/protos/method.proto
-go run *.go ./e2e/protos/dep1/dependency1.proto
-go run *.go ./e2e/protos/dep1/dep11/dependency11.proto
-go run *.go ./e2e/protos/dep2/dependent.proto
-go run *.go -m ./e2e/protos/service.proto
-go run *.go -m ./e2e/protos/service_no_serialization.proto
-go run *.go -m ./e2e/protos/no_serialization.proto
-go run *.go ./e2e/protos/crypto/aliases.proto
-go run *.go ./e2e/protos/aliases_user.proto
-go run *.go ./e2e/protos/unions.proto
-go run *.go -m ./e2e/protos/options/handlers/handlers.proto
-go run *.go -m ./e2e/protos/options/handlers.proto
+go run *.go -m `find . -name "*.proto"`
 go test ./e2e
 
 rm  `find . -name "*.mb.go"`
 
 packr install
-membufc --go --mock ./e2e/protos/*.proto
-membufc --go ./e2e/protos/dep1/*.proto
-membufc --go ./e2e/protos/dep1/dep11/*.proto
-membufc --go ./e2e/protos/dep2/*.proto
-membufc --go ./e2e/protos/crypto/*.proto
-membufc --go --mock ./e2e/protos/options/handlers/*.proto
-membufc --go --mock ./e2e/protos/options/*.proto
+membufc --go --mock `find . -name "*.proto"`
 go test ./e2e
--- go/test.sh
+++ go/test.sh
@@ -1,2 +1,4 @@
+go run ./membufc/*.go -m `find ./e2e/types -name "*.proto"`
+
 go test
 go test ./e2e
