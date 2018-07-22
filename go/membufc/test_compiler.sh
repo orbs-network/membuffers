c79e41a27bfec98c51a736d2801ca877bb71caa5
Tal Kol <talkol@gmail.com>
2018-07-22 09:39:52 +0300

    Fixed broken test script due to PR

--- go/membufc/test_compiler.sh
+++ go/membufc/test_compiler.sh
@@ -1,10 +1,30 @@
+echo "  * Running unit tests"
+echo ""
+
 go test .
+echo ""
 
-go run *.go -m `find . -name "*.proto"`
+echo "  * Building protos for tests (without building compiler)"
+echo ""
+
+go run $(ls -1 *.go | grep -v _test.go) -m `find . -name "*.proto"`
+
+echo "  * Running end to end tests"
+echo ""
+
 go test ./e2e
+echo ""
 
-rm  `find . -name "*.mb.go"`
+rm `find . -name "*.mb.go"`
+
+echo "  * Building protos for tests (with building compiler)"
+echo ""
 
 packr install
 `go env GOPATH`/bin/membufc --go --mock `find . -name "*.proto"`
+
+echo "  * Running end to end tests"
+echo ""
+
 go test ./e2e
+echo ""
--- go/test_lib.sh
+++ go/test_lib.sh
@@ -1,4 +1,10 @@
-go run ./membufc/*.go -m `find ./e2e/types -name "*.proto"`
+echo "  * Building protos for tests (without building compiler)"
+echo ""
+
+go run $(ls -1 ./membufc/*.go | grep -v _test.go) -m `find ./e2e/types -name "*.proto"`
+
+echo "  * Running tests"
+echo ""
 
 go test
 go test ./e2e
