b4384a68676e20d4c2fd24998247ddb26d69e05c
Tal Kol <talkol@gmail.com>
2018-10-10 09:12:42 +0300

    Add js builder for bytes and string

--- javascript/builder.js
+++ javascript/builder.js
@@ -1,5 +1,6 @@
-import {alignOffsetToType} from './message';
+import {alignOffsetToType, alignDynamicFieldContentOffset} from './message';
 import {FieldTypes, FieldSizes} from './types';
+import {getTextEncoder} from './text';
 
 export class InternalBuilder {
 
@@ -22,7 +23,7 @@ export class InternalBuilder {
 
   writeUint8(buf, v) {
     this.size = alignOffsetToType(this.size, FieldTypes.TypeUint8);
-    if (buf !== null) {
+    if (buf) {
       new DataView(buf.buffer).setUint8(this.size, v, true);
     }
     this.size += FieldSizes[FieldTypes.TypeUint8];
@@ -30,7 +31,7 @@ export class InternalBuilder {
 
   writeUint16(buf, v) {
     this.size = alignOffsetToType(this.size, FieldTypes.TypeUint16);
-    if (buf !== null) {
+    if (buf) {
       new DataView(buf.buffer).setUint16(this.size, v, true);
     }
     this.size += FieldSizes[FieldTypes.TypeUint16];
@@ -38,7 +39,7 @@ export class InternalBuilder {
 
   writeUint32(buf, v) {
     this.size = alignOffsetToType(this.size, FieldTypes.TypeUint32);
-    if (buf !== null) {
+    if (buf) {
       new DataView(buf.buffer).setUint32(this.size, v, true);
     }
     this.size += FieldSizes[FieldTypes.TypeUint32];
@@ -46,10 +47,34 @@ export class InternalBuilder {
 
   writeUint64(buf, v) {
     this.size = alignOffsetToType(this.size, FieldTypes.TypeUint64);
-    if (buf !== null) {
+    if (buf) {
       new DataView(buf.buffer).setBigUint64(this.size, v, true);
     }
     this.size += FieldSizes[FieldTypes.TypeUint64];
+  }
+
+  writeBytes(buf, v) {
+    const dataView = (buf) ? new DataView(buf.buffer) : undefined;
+    this.size = alignOffsetToType(this.size, FieldTypes.TypeBytes);
+    if (buf) {
+      if (v) {
+        dataView.setUint32(this.size, v.byteLength, true);
+      } else {
+        dataView.setUint32(this.size, 0, true);
+      }
+    }
+    this.size += FieldSizes[FieldTypes.TypeBytes];
+    this.size = alignDynamicFieldContentOffset(this.size, FieldTypes.TypeBytes);
+    if (v) {
+      if (buf) {
+        buf.set(v, this.size);
+      }
+      this.size += v.byteLength;
+    }
+  }
+
+  writeString(buf, v) {
+    this.writeBytes(buf, getTextEncoder().encode(v));
   }
 
 }
--- javascript/builder.test.js
+++ javascript/builder.test.js
@@ -1,4 +1,5 @@
 import './matcher-extensions';
+import {ch} from './text';
 import {InternalBuilder} from "./builder";
 
 test('TestBuilderUint8', () => {
@@ -47,4 +48,38 @@ test('TestBuilderUint64', () => {
   w.writeUint64(buf, v);
   const expected = new Uint8Array([0x17,0x00,0x00,0x00,0x00,0x00,0x00,0x00]);
   expect(buf).toBeEqualToUint8Array(expected);
+});
+
+test('TestBuilderBytes', () => {
+  const w = new InternalBuilder();
+  const v = new Uint8Array([0x01,0x02,0x03]);
+  w.writeBytes(null, v);
+  expect(w.size).toBe(7);
+  w.writeBytes(null, v);
+  expect(w.size).toBe(15);
+  const buf = new Uint8Array(w.size);
+  w.reset();
+  w.writeBytes(buf, v);
+  const expected = new Uint8Array([0x03,0x00,0x00,0x00, 0x01,0x02,0x03]);
+  expect(buf.subarray(0,7)).toBeEqualToUint8Array(expected);
+  w.writeBytes(buf, v);
+  const expected2 = new Uint8Array([0x03,0x00,0x00,0x00, 0x01,0x02,0x03,0x00, 0x03,0x00,0x00,0x00, 0x01,0x02,0x03,]);
+  expect(buf).toBeEqualToUint8Array(expected2);
+});
+
+test('TestBuilderString', () => {
+  const w = new InternalBuilder();
+  const v = "hello";
+  w.writeString(null, v);
+  expect(w.size).toBe(9);
+  w.writeString(null, v);
+  expect(w.size).toBe(21);
+  const buf = new Uint8Array(w.size);
+  w.reset();
+  w.writeString(buf, v);
+  const expected = new Uint8Array([0x05,0x00,0x00,0x00, ch('h'),ch('e'),ch('l'),ch('l'),ch('o')]);
+  expect(buf.subarray(0,9)).toBeEqualToUint8Array(expected);
+  w.writeString(buf, v);
+  const expected2 = new Uint8Array([0x05,0x00,0x00,0x00, ch('h'),ch('e'),ch('l'),ch('l'),ch('o'),0x00,0x00,0x00, 0x05,0x00,0x00,0x00, ch('h'),ch('e'),ch('l'),ch('l'),ch('o')]);
+  expect(buf).toBeEqualToUint8Array(expected2);
 });
