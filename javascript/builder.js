cc0d26de676afd4c93d38217b72a373ab38c7a71
Tal Kol <talkol@gmail.com>
2018-10-10 08:46:55 +0300

    Add js uint builder (16,32,64)

--- javascript/builder.js
+++ javascript/builder.js
@@ -23,9 +23,33 @@ export class InternalBuilder {
   writeUint8(buf, v) {
     this.size = alignOffsetToType(this.size, FieldTypes.TypeUint8);
     if (buf !== null) {
-      new DataView(buf.buffer).setUint8(this.size, v);
+      new DataView(buf.buffer).setUint8(this.size, v, true);
     }
     this.size += FieldSizes[FieldTypes.TypeUint8];
+  }
+
+  writeUint16(buf, v) {
+    this.size = alignOffsetToType(this.size, FieldTypes.TypeUint16);
+    if (buf !== null) {
+      new DataView(buf.buffer).setUint16(this.size, v, true);
+    }
+    this.size += FieldSizes[FieldTypes.TypeUint16];
+  }
+
+  writeUint32(buf, v) {
+    this.size = alignOffsetToType(this.size, FieldTypes.TypeUint32);
+    if (buf !== null) {
+      new DataView(buf.buffer).setUint32(this.size, v, true);
+    }
+    this.size += FieldSizes[FieldTypes.TypeUint32];
+  }
+
+  writeUint64(buf, v) {
+    this.size = alignOffsetToType(this.size, FieldTypes.TypeUint64);
+    if (buf !== null) {
+      new DataView(buf.buffer).setBigUint64(this.size, v, true);
+    }
+    this.size += FieldSizes[FieldTypes.TypeUint64];
   }
 
 }
--- javascript/builder.test.js
+++ javascript/builder.test.js
@@ -12,3 +12,39 @@ test('TestBuilderUint8', () => {
   const expected = new Uint8Array([0x17]);
   expect(buf).toBeEqualToUint8Array(expected);
 });
+
+test('TestBuilderUint16', () => {
+  const w = new InternalBuilder();
+  const v = 0x17;
+  w.writeUint16(null, v);
+  expect(w.size).toBe(2);
+  const buf = new Uint8Array(w.size);
+  w.reset();
+  w.writeUint16(buf, v);
+  const expected = new Uint8Array([0x17,0x00]);
+  expect(buf).toBeEqualToUint8Array(expected);
+});
+
+test('TestBuilderUint32', () => {
+  const w = new InternalBuilder();
+  const v = 0x17;
+  w.writeUint32(null, v);
+  expect(w.size).toBe(4);
+  const buf = new Uint8Array(w.size);
+  w.reset();
+  w.writeUint32(buf, v);
+  const expected = new Uint8Array([0x17,0x00,0x00,0x00]);
+  expect(buf).toBeEqualToUint8Array(expected);
+});
+
+test('TestBuilderUint64', () => {
+  const w = new InternalBuilder();
+  const v = BigInt(0x17);
+  w.writeUint64(null, v);
+  expect(w.size).toBe(8);
+  const buf = new Uint8Array(w.size);
+  w.reset();
+  w.writeUint64(buf, v);
+  const expected = new Uint8Array([0x17,0x00,0x00,0x00,0x00,0x00,0x00,0x00]);
+  expect(buf).toBeEqualToUint8Array(expected);
+});
