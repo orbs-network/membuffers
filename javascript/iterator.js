d8b55355b285960651b966913ed67cba059aeb3b
Tal Kol <talkol@gmail.com>
2019-03-24 18:38:48 +0200

    Added license files

--- javascript/iterator.js
+++ javascript/iterator.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2018 the membuffers authors
+ * This file is part of the membuffers library in the Orbs project.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+ * The above notice should be included in all copies or substantial portions of the software.
+ */
+
 import {alignDynamicFieldContentOffset} from './message';
 import {FieldTypes, FieldSizes} from './types';
 import {getTextDecoder} from "./text";
--- javascript/matcher-extensions.js
+++ javascript/matcher-extensions.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright 2018 the membuffers authors
+ * This file is part of the membuffers library in the Orbs project.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+ * The above notice should be included in all copies or substantial portions of the software.
+ */
+
 function arrayBufferToHex(buff) {
   return Array.prototype.map.call(new Uint8Array(buff), x => ('00' + x.toString(16)).slice(-2)).join('');
 }
