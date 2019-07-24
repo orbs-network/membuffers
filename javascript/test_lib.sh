9dec80139b316ccde41771bea91b05183aa4589a
itamar Arjuan <itamar.arjuan@gmail.com>
2019-07-24 16:38:45 +0300

    fixing GOBIN path to allow installation of packr correctly

--- javascript/test_lib.sh
+++ javascript/test_lib.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 npm test
 
 echo "  /"
