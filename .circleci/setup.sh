37e37caf9808dfd33bd294848cff6842cbafee08
itamar Arjuan <itamar.arjuan@gmail.com>
2019-07-24 16:16:28 +0300

    switching back to sh

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd javascript && npm install
 cd ..
 go get -u github.com/gobuffalo/packr/packr
