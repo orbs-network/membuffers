f05c7cda53d5706514bb4a9458e43d2de5957597
Kirill Maksimov <kirill@orbs.com>
2019-10-29 12:39:59 +0200

    Another iOS bugfix

--- javascript/src/data-view-wrapper.ts
+++ javascript/src/data-view-wrapper.ts
@@ -1,31 +1,42 @@
-import jDataView from "jdataview";
-import { wrapInt64, wrapUint64, unwrapInt64, unwrapUint64 } from "./jdataview-bigint";
+// import jDataView, { Uint64 } from "jdataview";
+// import { wrapInt64, wrapUint64, unwrapInt64, unwrapUint64 } from "./jdataview-bigint";
+import { uint64ToUint8Array, uint8ArrayToUint64 } from "./bigint";
 
 let wrapper: any = DataView;
 
-class JDataViewImplemetation extends jDataView {
-    setBigInt64(byteOffset: number, value: bigint, littleEndian?: boolean): void {
-        this.setInt64(byteOffset, wrapInt64(value), littleEndian)
-    }
+class IOSDataViewImplemetation extends DataView {
+    OFFSET_32 = 4
 
     setBigUint64(byteOffset: number, value: bigint, littleEndian?: boolean): void {
-        this.setUint64(byteOffset, wrapUint64(value), littleEndian)
-    }
-
-    getBigInt64(byteOffset?: number, littleEndian?: boolean): bigint {
-        return unwrapInt64(this.getInt64(byteOffset, littleEndian));
+        let arr = uint64ToUint8Array(value);
+        if (littleEndian) {
+           arr = arr.reverse();
+        }
+    
+        for (let i = 0; i < arr.length ; i++) {
+            this.setUint8(byteOffset+i, arr[i])
+        }
     }
     
     getBigUint64(byteOffset?: number, littleEndian?: boolean): bigint {
-        return unwrapUint64(this.getUint64(byteOffset, littleEndian))
+        let arr = new Uint8Array(8);
+        for (let i = 0; i < arr.length ; i++) {
+            arr[i] = this.getUint8(byteOffset+i)
+        }
+        
+        if (littleEndian) {
+            arr = arr.reverse();
+        }
+
+        return uint8ArrayToUint64(arr);
     }
 }
 
 try {
     new DataView(new ArrayBuffer(8)).setBigUint64(0, BigInt(0));
 } catch (e) {
-    wrapper = JDataViewImplemetation;
+    wrapper = IOSDataViewImplemetation;
 }
 
 export const DataViewWrapper: DataViewConstructor = <DataViewConstructor>wrapper;
-export const JDataViewWrapper: DataViewConstructor = <DataViewConstructor><unknown>JDataViewImplemetation;
+export const IOSDataViewWrapper: DataViewConstructor = <DataViewConstructor><unknown>IOSDataViewImplemetation;
